f2cb4b690eeb3b53b8a8764bcb0d48d7f0503355
Tuğba Cinbek <tugbacinbek@gmail.com>
2024-03-01 23:57:38 +0300

    Loops

---
+++ Loops/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ForLoop();
+
+
+            if (IsPrimeNumber(6))
+            {
+                Console.WriteLine("This is a prime number");
+            }
+            else
+            {
+                Console.WriteLine("This is not a prime number");
+            }
+
+            Console.ReadLine();
+        }
+        private static bool IsPrimeNumber(int number)
+        {
+            bool result = true;
+            for (int i = 2; i < number-1; i++)
+            {
+                if (number%i==0)
+                {
+                    result = false;
+                    i = number;
+                }
+
+            }
+            return result;
+        }
+
+        private static void ForLoop()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Finished");
+        }
+
+        // Aşağıda 100'den geriye ikişer ikişer azalıyor sıfıra kadar.
+        //static void Main(string[] args)
+        //{
+        //    for(int i = 100; i >= 0; i=i-2)
+        //    { 
+        //        Console.WriteLine(i);
+        //    }
+
+        
+        }
+    }
+

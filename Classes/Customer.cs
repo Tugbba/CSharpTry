271e824675637a65c86ea8f40431d68e30b72c13
Tuğba Cinbek <tugbacinbek@gmail.com>
2024-03-02 15:58:24 +0300

    Classes

---
+++ Classes/Customer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Customer
+    {
+        public int Id { get; set; }
+
+      //Eğer ki müşteri ismnini giriyor(set) ve biz ona hitap etmek için isminin başına sözcük eklemek(get) istiyoruz. Bunun için aşağıdaki gibi yazarız.
+      //Ama genelde böyle bir kullanım yok eskiden kullanılırdı.
+        private string _firstName;
+        public string FirstName 
+        { 
+            get { return "Mr." + _firstName; }
+            set { _firstName = value; }
+        }
+        public string LastName { get; set; }
+        public string City { get; set; }
+    }
+}
---
+++ Classes/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add();
+            customerManager.Update();
+
+            ProductManager productManager = new ProductManager();
+            productManager.Add();
+            productManager.Update();
+
+            Customer customer = new Customer();
+            customer.City = "Ankara";
+            customer.Id = 1;
+            customer.FirstName = "Tuğba";
+            customer.LastName = "Cinbek";
+
+            Customer customer2 = new Customer
+            {
+                Id = 2, City =  "İstanbul", FirstName = "Serkan", LastName ="Sağlam"
+            };
+
+            Console.WriteLine(customer2.FirstName);
+            Console.ReadLine();
+        }
+    }
+}
+      
+//Class oluşturduktan sonra onun çalışması için onu Main de çağırmamız lazım yani ilk newliycez sonra içindeki metotları çağıracağız.
+    
+
+    
+
+    
